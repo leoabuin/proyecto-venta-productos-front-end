e25d52a6336bf89b1f692f8270f1164682bec60e
Leonel Abuin <leonelabuin2002@gmail.com>
2024-09-06 19:05:14 -0300

    creacion componentes: log-in, home. Implementacion de Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { LogInComponent } from './log-in/log-in.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent,LogInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
