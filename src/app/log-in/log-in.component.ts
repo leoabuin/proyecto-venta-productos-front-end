3f1f6b1285331d5765b5488784b3e57f19c19188
Leonel Abuin <leonelabuin2002@gmail.com>
2024-09-13 17:41:26 -0300

    creacion de l componente del log-in

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
-
+  mail: string = '';
 }
