1f51ba869f60f38726431ca5858b7b0cf5947fb9
TomasHidalgo2024 <tomashidal70@gmail.com>
2024-09-22 15:31:19 -0300

    Pulido del Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component.js";
 import { LogInComponent } from './log-in/log-in.component.js';
+import { NosotrosComponent } from "./nosotros/nosotros.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent,LogInComponent],
+  imports: [RouterOutlet, HomeComponent, LogInComponent, NosotrosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
