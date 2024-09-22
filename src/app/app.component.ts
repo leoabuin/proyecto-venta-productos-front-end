415f239724ef18fd814917091d70af32a6019124
TomasHidalgo2024 <tomashidal70@gmail.com>
2024-09-22 20:42:56 -0300

    Formulario de Inscripcion de Marca.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component.js";
 import { LogInComponent } from './log-in/log-in.component.js';
 import { NosotrosComponent } from "./nosotros/nosotros.component";
+import { GamingComponent } from "./gaming/gaming.component";
+import { AddcategoriesComponent } from "./addcategories/addcategories.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, LogInComponent, NosotrosComponent],
+  imports: [RouterOutlet, HomeComponent, LogInComponent, NosotrosComponent, GamingComponent, AddcategoriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
