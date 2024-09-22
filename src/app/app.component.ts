bc16a3b757abf425be1799de4834a48c35e0fd92
Leonel Abuin <leonelabuin2002@gmail.com>
2024-09-22 19:15:08 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component.js";
 import { LogInComponent } from './log-in/log-in.component.js';
+import { BrandComponent } from './brand/brand.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent,LogInComponent],
+  imports: [RouterOutlet, HomeComponent,LogInComponent,BrandComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
