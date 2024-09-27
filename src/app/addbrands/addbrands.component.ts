018670e75c2fdf977d475541d15000f8664bed67
Leonel Abuin <leonelabuin2002@gmail.com>
2024-09-27 17:34:31 -0300

    implements addBrand

--- src/app/addbrands/addbrands.component.ts
+++ src/app/addbrands/addbrands.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../service/api.service.js';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-addbrands',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './addbrands.component.html',
   styleUrl: './addbrands.component.scss'
 })
 export class AddbrandsComponent {
   
+  brandData = {
+    name: '',
+    description: '',
+    website:'',
+    countryOfOrigin:'',
+    logo:''
+  };
 
+  constructor(private service: ApiService){}
+
+  submitForm() {
+    this.service.createBrand(this.brandData).subscribe({
+      next: (response) => {
+        console.log('Marca agregada:', response);
+        // Aquí puedes agregar lógica adicional, como limpiar el formulario
+      },
+      error: (error) => {
+        console.error('Error al agregar la marca:', error);
+      }
+    });
+  }
 
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -15,4 +15,8 @@ export class ApiService {
   public getData(): Observable<any>{
     return this.http.get<any>(this.urlBrandApi)
   }
+
+  public createBrand(brandData: any): Observable<any> {
+    return this.http.post<any>(this.urlBrandApi, brandData);
+  }
 }
