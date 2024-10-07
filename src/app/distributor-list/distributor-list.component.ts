e0c5defe0e21a072386990fc7cc778bc2d2f00b2
TomasHidalgo2024 <tomashidal70@gmail.com>
2024-10-06 21:39:53 -0300

    Componente NavBar y Distributor List.

---
+++ src/app/distributor-list/distributor-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-distributor-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './distributor-list.component.html',
+  styleUrl: './distributor-list.component.scss'
+})
+export class DistributorListComponent {
+
+}
