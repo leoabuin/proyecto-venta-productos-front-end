7d0bdb06f51f9c2934cb3ca54a62e458a196eafb
Lucas Santiago <03santiagolucas@gmail.com>
2024-11-13 19:12:08 -0300

    Se creo y estilizo el componente de carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component.js";
+import { HomeComponent } from './home/home.component.js';
 import { LogInComponent } from './log-in/log-in.component.js';
 import { AddbrandsComponent } from './addbrands/addbrands.component.js';
 import { HttpClient } from '@angular/common/http';
 import { provideHttpClient } from '@angular/common/http';
-import { NosotrosComponent } from "./nosotros/nosotros.component";
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
-import { AddcategoriesComponent } from "./addcategories/addcategories.component";
+import { AddcategoriesComponent } from './addcategories/addcategories.component';
 import { BrandListComponent } from './brand-list/brand-list.component.js';
-import { CategoriesListComponent } from "./categories-list/categories-list.component";
+import { CategoriesListComponent } from './categories-list/categories-list.component';
 import { DistributorListComponent } from './distributor-list/distributor-list.component.js';
 import { NavbarComponent } from '../navbar/navbar.component.js';
 import { ProductsListComponent } from './products-list/products-list.component.js';
 import { AddProductComponent } from './add-product/add-product.component.js';
 import { ProductDetailsComponent } from './product-details/product-details.component.js';
-import { ShoppingCartComponent } from "./shopping-cart/shopping-cart.component";
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { UpdateCategoryComponent } from './update-category/update-category.component.js';
 import { ChangePriceComponent } from './change-price/change-price.component.js';
 import { RegisterUserComponent } from './register-user/register-user.component.js';
 import { FooterComponent } from './footer/footer.component.js';
 import { MyOrdersComponent } from './my-orders/my-orders.component.js';
+import { CarouselComponent } from './carousel/carousel.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
+  imports: [
+    RouterOutlet,
     HomeComponent,
     LogInComponent,
     NosotrosComponent,
@@ -37,16 +42,16 @@ import { MyOrdersComponent } from './my-orders/my-orders.component.js';
     DistributorListComponent,
     ProductsListComponent,
     AddProductComponent,
-    ProductDetailsComponent, 
+    ProductDetailsComponent,
     ShoppingCartComponent,
     UpdateCategoryComponent,
     ChangePriceComponent,
     RegisterUserComponent,
     FooterComponent,
-    MyOrdersComponent
+    MyOrdersComponent,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'proyecto-venta-productos-front-end';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from "../../navbar/navbar.component";
+import { NavbarComponent } from '../../navbar/navbar.component';
 import { LocalStorageService } from '../service/local-storage.service.js';
+import { FooterComponent } from '../footer/footer.component';
+import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, RouterModule, NavbarComponent,NavbarComponent],
+  imports: [
+    FormsModule,
+    RouterModule,
+    NavbarComponent,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  username: string | null = null
-
-
-
+  username: string | null = null;
 
   constructor(private localStorageService: LocalStorageService) {
     this.username = this.localStorageService.getItem('username');
-    console.log(this.username); 
+    console.log(this.username);
   }
-
 }
