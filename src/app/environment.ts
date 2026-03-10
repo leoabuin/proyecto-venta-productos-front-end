93a70a7464c03f280784ec34c3ec1ece3d5da6b1
Leonel <leonelabuin2002@gmail.com>
2026-03-09 23:06:10 -0300

    Frontend: environments incluidos y gitignore limpio

---
+++ src/app/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api', // La que usás en tu PC
+  emailjs_service_id: 'service_cr3ijnk',
+  emailjs_template_id: 'template_fj2vbvt',
+  emailjs_public_key: 'xiQ8aWHYAIc_dnEik'
+};
