1973e2fb706371bc1490e3fa732be8dd6ab273cb
Lucas Santiago <03santiagolucas@gmail.com>
2026-04-08 19:55:54 -0300

    Solución de errores: puerto de la API y advertencia de hidratación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { AuthInterceptor } from './service/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
-    provideHttpClient(),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    }
+    provideHttpClient(withFetch())
   ]
 };
