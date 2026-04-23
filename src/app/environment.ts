64280af96b79601af2f8397105a4567e3634bfb1
Lucas Santiago <03santiagolucas@gmail.com>
2026-04-22 21:02:05 -0300

    Deploy en railway

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api', // La que usás en tu PC
+  apiUrl: 'https://proyecto-venta-productos-production-6859.up.railway.app/api', // URL de producción en Railway
   emailjs_service_id: 'service_cr3ijnk',
   emailjs_template_id: 'template_fj2vbvt',
   emailjs_public_key: 'xiQ8aWHYAIc_dnEik'
