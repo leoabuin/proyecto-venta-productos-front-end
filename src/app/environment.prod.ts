dea85c7de5bcf5599b2a1eadefc97b081f8f12d4
Lucas Santiago <03santiagolucas@gmail.com>
2026-04-22 21:33:56 -0300

    Correccion env para registros

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true, // Cambiamos a true
-  apiUrl: 'https://proyecto-venta-productos-production.up.railway.app/api', // URL de tu backend en Railway
+  apiUrl: 'https://proyecto-venta-productos-production-6859.up.railway.app/api', // URL de tu backend en Railway
   emailjs_service_id: 'service_cr3ijnk',
   emailjs_template_id: 'template_fj2vbvt',
   emailjs_public_key: 'xiQ8aWHYAIc_dnEik'
